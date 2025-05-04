3ed8feafcff802cb05df4e48716216d29cd9151c
jiheon2234 <jiheon2234@gmail.com>
2025-05-04 22:26:01 +0900

    기본설정

---
+++ src/main/java/com/happypill/HappypillApplication.java
@@ -0,0 +1,13 @@
+package com.happypill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappypillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappypillApplication.class, args);
+    }
+
+}
