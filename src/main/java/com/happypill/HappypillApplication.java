7480eddc462a396e5ebb0973533c9f7c05bca562
Jeremy <jamkim1997@gmail.com>
2025-05-18 10:18:48 +1000

    HP-39 : 풀리퀘스트 양식 적용 확인

--- src/main/java/com/happypill/HappypillApplication.java
+++ src/main/java/com/happypill/HappypillApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HappypillApplication {
-
+    // Test
     public static void main(String[] args) {
         SpringApplication.run(HappypillApplication.class, args);
     }
