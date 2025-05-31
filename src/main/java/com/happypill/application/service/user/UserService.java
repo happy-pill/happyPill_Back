4cf5ee865f6a8c9198aa7346bd6f2bf99fa13ce0
jiheon2234 <hello@jiheon2234.dev>
2025-05-31 23:57:02 +0900

    HP-66 전역 예외처리 리베이스 후, 도메인 에러로 변경

--- src/main/java/com/happypill/application/service/user/UserService.java
+++ src/main/java/com/happypill/application/service/user/UserService.java
@@ -2,6 +2,7 @@ package com.happypill.application.service.user;
 
 import com.happypill.application.auth.UserContext;
 import com.happypill.application.entity.HappypillUser;
+import com.happypill.application.exception.global.BusinessException;
 import com.happypill.application.repository.happypilluser.HappypillUserRepository;
 import com.happypill.application.service.user.request.UserUpdateRequest;
 import com.happypill.application.service.user.response.UserInfoResponse;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.happypill.application.exception.custom.ExceptionCode.USER_NOT_FOUND;
 
 @Service
 @Slf4j
@@ -19,14 +22,14 @@ public class UserService {
 
     public UserInfoResponse getCurrentUserInfo(UserContext userContext) {
         HappypillUser user = userRepository.findById(userContext.getId())
-                .orElseThrow(() -> new RuntimeException("추후 도메인 에러로 변경"));
+                .orElseThrow(() -> new BusinessException(USER_NOT_FOUND));
         return UserInfoResponse.from(user);
     }
 
     @Transactional
     public UserInfoResponse updateCurrentUser(UserContext userContext, UserUpdateRequest userUpdateRequest) {
         HappypillUser user = userRepository.findById(userContext.getId())
-                .orElseThrow(() -> new RuntimeException("추후 도메인 에러로 변경"));
+                .orElseThrow(() -> new BusinessException(USER_NOT_FOUND));
         user.changeUser(userUpdateRequest.nickName());
         return UserInfoResponse.from(user);
     }
