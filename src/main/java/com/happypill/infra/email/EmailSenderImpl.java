f0ca12f873f42b36a32db651d6d79bc21a94a722
DongHun Choi <cdh3946@gmail.com>
2025-06-01 22:03:35 +0900

    fix : 구현체 분리

--- src/main/java/com/happypill/infra/email/EmailSenderImpl.java
+++ src/main/java/com/happypill/infra/email/EmailSenderImpl.java
@@ -1,5 +1,6 @@
-package com.happypill.application.email;
+package com.happypill.infra.email;
 
+import com.happypill.application.email.EmailSender;
 import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.InternetAddress;
@@ -23,17 +24,14 @@ public class EmailSenderImpl implements EmailSender {
     @Override
     public void send(String subject, String content) {
         try{
-
             MimeMessage message = javaMailSender.createMimeMessage();
             message.setRecipients(Message.RecipientType.TO, subject);
             message.setSubject("해피필 인증코드");
             message.setText(content, "utf-8", "html");
             message.setFrom(new InternetAddress(senderEmail, "HappyPill"));
             javaMailSender.send(message);
-
         }catch (MessagingException | UnsupportedEncodingException e){
 
         }
-
     }
 }
