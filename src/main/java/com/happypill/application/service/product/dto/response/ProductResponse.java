a36707c6a9e98e8edf3577d379e63b00052ac9e3
Jeremy <66817314+jamkim1997@users.noreply.github.com>
2025-06-08 21:47:10 +1000

    HP-53: ID를 Long을 String으로 변환 (#33)

--- src/main/java/com/happypill/application/service/product/dto/response/ProductResponse.java
+++ src/main/java/com/happypill/application/service/product/dto/response/ProductResponse.java
@@ -4,14 +4,14 @@ import com.happypill.application.entity.Category;
 import com.happypill.application.entity.Product;
 import com.happypill.application.entity.ProductInfo;
 
-public record ProductResponse(Long productId, Long categoryId, String name, String company, int price,
+public record ProductResponse(String productId, Long categoryId, String name, String company, int price,
                               String briefDescription, String thumbnailUrl)
 {
     public static ProductResponse from(ProductInfo productInfo, Integer price) {
         Product product = productInfo.getProduct();
         Category category = product.getCategory();
 
-        return new ProductResponse(product.getProductId(), category.getCategoryId(), productInfo.getName(),
+        return new ProductResponse(product.getProductId().toString(), category.getCategoryId(), productInfo.getName(),
                 productInfo.getCompany(), price, productInfo.getBriefDescription(), product.getThumbnailUrl());
     }
 }
