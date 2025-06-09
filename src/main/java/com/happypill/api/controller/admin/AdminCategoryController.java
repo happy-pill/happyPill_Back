aaf9179e3de66f33ab0f5ccfc2ec49f51c392a89
Choi DongHun <cdh3946@gmail.com>
2025-06-09 18:12:44 +0900

    HP-89 : [관리자] 모든 카테고리 조회 endpoint (#32)
    * HP-89 : [관리자] 모든 카테고리 조회 endpoint
    
    * fix : dto에서 Long 타입 String 으로 변경
    
    * fix : 테스트 코드 beforeEach setUp 삭제
    
    * fix : 테스트 코드 Language 부분 수정

---
+++ src/main/java/com/happypill/api/controller/admin/AdminCategoryController.java
@@ -0,0 +1,32 @@
+package com.happypill.api.controller.admin;
+
+import com.happypill.application.pagination.CustomPage;
+import com.happypill.application.service.admin.AdminCategoryService;
+import com.happypill.application.service.admin.response.AdminCategoryListResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin/categories")
+public class AdminCategoryController {
+
+    private static final String LANGUAGE_HEADER = "Language";
+
+    private final AdminCategoryService adminCategoryService;
+
+    //모든 카테고리 조회
+    @GetMapping
+    //TODO : 추가 예정 @PreAuthorize("hasRole('ADMIN')")
+    public CustomPage<AdminCategoryListResponse> getCategories(@RequestParam(value = "page", defaultValue = "1") int page,
+                                                               @RequestParam(value = "size", defaultValue = "5") int size,
+                                                               @RequestHeader(LANGUAGE_HEADER) String headerLanguage) {
+        Locale locale = Locale.forLanguageTag(headerLanguage);
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return adminCategoryService.getAllCategories(pageable, locale);
+    }
+}
--- src/main/java/com/happypill/application/repository/categoryinfo/CategoryInfoRepository.java
+++ src/main/java/com/happypill/application/repository/categoryinfo/CategoryInfoRepository.java
@@ -2,10 +2,21 @@ package com.happypill.application.repository.categoryinfo;
 
 import com.happypill.application.entity.CategoryInfo;
 import com.happypill.application.entity.enums.Language;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CategoryInfoRepository extends JpaRepository<CategoryInfo, Long> {
     List<CategoryInfo> findByLanguage(Language language);
+
+    @Query("""
+        SELECT ci
+        FROM CategoryInfo ci
+        WHERE ci.language = :language
+""")
+    Page<CategoryInfo> getAllCategoryInfo(Pageable pageable, @Param("language") Language language);
 }
