c5d1e502279437e8fe29909e767dbc9600e928a5
DongHun Choi <cdh3946@gmail.com>
2025-06-11 18:25:48 +0900

    fix : dto nickName 유효성 수정

--- src/main/java/com/happypill/application/service/admin/request/AdminUserUpdateRequest.java
+++ src/main/java/com/happypill/application/service/admin/request/AdminUserUpdateRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 
 public record AdminUserUpdateRequest(
-        @Size(min = 4, max = 10, message = "닉네임은 최소 4자 이상 10자 이하여야 합니다.")
+        @Size(min = 2, max = 10, message = "닉네임은 최소 2자 이상 10자 이하여야 합니다.")
         String nickName,
 
         @Email(message = "이메일 형식이어야 합니다.")
