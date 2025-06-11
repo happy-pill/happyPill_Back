40cd53fd1c6f12fc93468f44a247bbecaccfb5d6
DongHun Choi <cdh3946@gmail.com>
2025-06-11 20:31:55 +0900

    HP-97 : SnowFlakeId to String

--- src/main/java/com/happypill/application/service/admin/response/AdminProductPriceResponse.java
+++ src/main/java/com/happypill/application/service/admin/response/AdminProductPriceResponse.java
@@ -2,18 +2,17 @@ package com.happypill.application.service.admin.response;
 
 import com.happypill.application.entity.ProductPrice;
 
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public record AdminProductPriceResponse(
-        String date,
+        ZonedDateTime date,
         Integer price,
         boolean isUsed
 ) {
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
     public static AdminProductPriceResponse from(ProductPrice price){
-        String formattedDate = price.getCreatedAt().format(formatter);
         return new AdminProductPriceResponse(
-                formattedDate,
+                price.getCreatedAt(),
                 price.getPrice(),
                 price.isUsed()
         );
