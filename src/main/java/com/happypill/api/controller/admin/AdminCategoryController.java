5275ba832f87d7f715040797cf56f344d27b1db1
DongHun Choi <cdh3946@gmail.com>
2025-06-15 23:13:03 +0900

    fix : conflict 해결 중 한글 깨짐 문제

--- src/main/java/com/happypill/api/controller/admin/AdminCategoryController.java
+++ src/main/java/com/happypill/api/controller/admin/AdminCategoryController.java
@@ -4,6 +4,7 @@ import com.happypill.application.pagination.CustomPage;
 import com.happypill.application.service.admin.AdminCategoryService;
 import com.happypill.application.service.admin.response.AdminCategoryListResponse;
 import com.happypill.application.swagger.AuthFailureResponses;
+import com.happypill.application.swagger.OKAndServerErrorResponses;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -27,9 +28,9 @@ public class AdminCategoryController {
 
     @Operation(summary = "모든 카테고리 조회", description = "카테고리 리스트를 조회합니다.")
     @AuthFailureResponses
-    @ApiResponse(responseCode = "200", description = "요청이 정상적으로 처리되었을 때")
+    @OKAndServerErrorResponses
     @GetMapping
-    @PreAuthorize("hasRole('ADMIN')")
+    //TODO : 추가 예정 @PreAuthorize("hasRole('ADMIN')")
     public CustomPage<AdminCategoryListResponse> getCategories(@RequestParam(value = "page", defaultValue = "1") int page,
                                                                @RequestParam(value = "size", defaultValue = "5") int size,
                                                                @RequestHeader(LANGUAGE_HEADER) String headerLanguage) {
