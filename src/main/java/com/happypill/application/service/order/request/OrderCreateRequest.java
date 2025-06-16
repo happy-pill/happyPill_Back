09da0adcc01cc4099ec7cc8e931e4965be43850d
jiheon2234 <me@j2234.dev>
2025-06-16 19:14:30 +0900

    HP-92/Order 작업중 수신인 이름, 전화번호 검증 추가

--- src/main/java/com/happypill/application/service/order/request/OrderCreateRequest.java
+++ src/main/java/com/happypill/application/service/order/request/OrderCreateRequest.java
@@ -1,17 +1,19 @@
 package com.happypill.application.service.order.request;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.Range;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public record OrderCreateRequest(
+
+        @NotBlank(message = "수신인 이름은 필수입니다")
         String recipentName,
+
+        @NotBlank(message = "수신인 전화번호는 필수입니다")
+        @Pattern(regexp = "^010\\d{7,8}$", message = "휴대폰 번호 형식이 올바르지 않습니다.")
         String recipentMobile,
 
         @NotEmpty
