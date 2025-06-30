58c7731f72b33ca4dc52625a787191810f9beff1
DongHun Choi <cdh3946@gmail.com>
2025-06-30 19:29:42 +0900

    fix : Q클래스 static import 적용

--- src/main/java/com/happypill/application/repository/subscription/SubscriptionRepositoryImpl.java
+++ src/main/java/com/happypill/application/repository/subscription/SubscriptionRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.happypill.application.repository.subscription;
 
-import com.happypill.application.entity.*;
 import com.happypill.application.entity.enums.Language;
 import com.happypill.application.entity.enums.OrderStatus;
 import com.happypill.application.service.admin.response.AdminSubscriptionListResponse;
@@ -14,52 +13,54 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static com.happypill.application.entity.QHappypillUser.happypillUser;
+import static com.happypill.application.entity.QOrder.order;
+import static com.happypill.application.entity.QOrderLine.orderLine;
+import static com.happypill.application.entity.QProduct.product;
+import static com.happypill.application.entity.QProductInfo.productInfo;
+import static com.happypill.application.entity.QSubscription.subscription;
+
 @Repository
 @RequiredArgsConstructor
 public class SubscriptionRepositoryImpl implements SubscriptionRepositoryCustom{
 
     private final JPAQueryFactory jpaQueryFactory;
+
     @Override
     public Page<AdminSubscriptionListResponse> findSubscriptionsByLanguageAndCompletionAndStatus(Language language, boolean isCompleted, OrderStatus orderStatus, Pageable pageable) {
-        QSubscription s = QSubscription.subscription;
-        QOrderLine ol = QOrderLine.orderLine;
-        QHappypillUser u = QHappypillUser.happypillUser;
-        QOrder o = QOrder.order;
-        QProduct p = QProduct.product;
-        QProductInfo pi = QProductInfo.productInfo;
 
         //모든 구독 상품 조회 쿼리
         List<AdminSubscriptionListResponse> content = jpaQueryFactory
                 .select(new QAdminSubscriptionListResponse(
-                        pi.name,
-                        u.notifyEmail,
-                        s.id.stringValue(),
-                        s.nextDeliveryDate
+                        productInfo.name,
+                        happypillUser.notifyEmail,
+                        subscription.id.stringValue(),
+                        subscription.nextDeliveryDate
                 ))
-                .from(s)
-                .join(s.orderLine, ol)
-                .join(s.user, u)
-                .join(ol.order, o)
-                .join(ol.product, p)
-                .join(pi).on(pi.product.eq(p).and(pi.language.eq(language)))
+                .from(subscription)
+                .join(subscription.orderLine, orderLine)
+                .join(subscription.user, happypillUser)
+                .join(orderLine.order, order)
+                .join(orderLine.product, product)
+                .join(productInfo).on(productInfo.product.eq(product).and(productInfo.language.eq(language)))
                 .where(
-                        s.isCompleted.eq(isCompleted),
-                        o.status.eq(orderStatus)
+                        subscription.isCompleted.eq(isCompleted),
+                        order.status.eq(orderStatus)
                 )
-                .orderBy(s.id.desc())
+                .orderBy(subscription.id.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
         // 전체 카운트 쿼리 (페이지네이션을 위한 쿼리)
         Long total = jpaQueryFactory
-                .select(s.count())
-                .from(s)
-                .join(s.orderLine, ol)
-                .join(ol.order, o)
+                .select(subscription.count())
+                .from(subscription)
+                .join(subscription.orderLine, orderLine)
+                .join(orderLine.order, order)
                 .where(
-                        s.isCompleted.eq(isCompleted),
-                        o.status.eq(orderStatus)
+                        subscription.isCompleted.eq(isCompleted),
+                        order.status.eq(orderStatus)
                 )
                 .fetchOne();
 
